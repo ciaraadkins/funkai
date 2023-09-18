28f3d30e515fd305bd3837fff37812d6de3b9bdb
Ciara Adkins <ciaraadkins@WTMBP-Ciara-Adkins.local>
2023-09-18 10:14:32 -0400

    added setup class to import in init file

--- funkai/__init__.py
+++ funkai/__init__.py
@@ -1,1 +1,1 @@
-from .core import funktion, add_funk, update_funk, remove_funk, funk_details, approx_cost
+from .core import funktion, add_funk, update_funk, remove_funk, funk_details, approx_cost,setup

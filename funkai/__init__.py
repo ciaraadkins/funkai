41d5d56282c27a541eb02f893ab14f3638750859
Ciara Adkins <ciaraadkins@WTMBP-Ciara-Adkins.local>
2023-09-25 12:50:09 -0400

    updated init to match new architecture

--- funkai/__init__.py
+++ funkai/__init__.py
@@ -1,1 +1,2 @@
-from .core import funktion, add_funk, update_funk, remove_funk, funk_details, approx_cost, Setup
+from .core import Funk, FunkManager
+# from .core import funktion, add_funk, update_funk, remove_funk, funk_details, approx_cost, Setup
